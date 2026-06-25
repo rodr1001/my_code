4d40606d1b695ef419665c51d9036e6feee09cb1
rodr1001 <rodr1001@umn.edu>
2026-06-25 13:17:25 -0500

    update ana cfg with script to auto-tag long runs

--- ldmx/EAT_Tracking/track_ana_cfg.py
+++ ldmx/EAT_Tracking/track_ana_cfg.py
@@ -1,6 +1,10 @@
 from LDMX.Framework import ldmxcfg
 p= ldmxcfg.Process("ana")
 p.sequence = [ldmxcfg.Analyzer.from_file('TrackAnalyzer.cxx')]
-import sys
-p.inputFiles = sys.argv[1:]
-p.histogramFile = f'hist_track_ana_{len(p.inputFiles)}.root'
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--run-tag', default="TEST", help='extra name to include in output file')
+parser.add_argument('input_files', nargs='+', help='input root files')
+args = parser.parse_args()
+p.inputFiles = args.input_files
+p.histogramFile = f'hist_track_ana_{len(p.inputFiles)}_{args.run_tag}.root'
